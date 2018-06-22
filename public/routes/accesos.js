4c656202fcea56173c1cef260c28b77cf77e9525
Pepe Valdivia <jvaldivia@softweb.pe>
2018-06-22 12:27:04 -0500

    Archivos estáticos de gestión de usuarios

--- public/routes/accesos.js
+++ public/routes/accesos.js
@@ -7,6 +7,11 @@ var accesosRouter = Backbone.Router.extend({
   sistemaMenuView: null,
   sistemaPermisoView: null,
   sistemaRolView: null,
+  usuarioView: null,
+  usuarioLogView: null,
+  usuarioDetalleView: null,
+  usuarioSistemaView: null,
+  usuarioRolPermisoView: null,
   initialize: function() {
   },
   routes: {
@@ -17,8 +22,8 @@ var accesosRouter = Backbone.Router.extend({
     "sistema/rol/:sistema_id" : "sistemaRol",
     "usuario/logs/:usuario_id" : "usuarioLog",
     "usuario/editar/:usuario_id" : "usuarioDetalle", 
-    "usuario/sistemas/:usuario_id" : "usuarioSistemas", 
-    "usuario/roles_permisos/:usuario_id" : "usuarioRolesPermisos", 
+    "usuario/sistema/:usuario_id" : "usuarioSistema", 
+    "usuario/roles_permisos/:usuario_id" : "usuarioRolPermiso", 
     "usuario" : "usuarioIndex",
     "*actions" : "default",
   },
@@ -28,6 +33,7 @@ var accesosRouter = Backbone.Router.extend({
   default: function() {
     //window.location.href = BASE_URL + "error/access/404";
   },
+  //sistema
   sistemaIndex: function(){
     if(this.sistemaView == null){
       this.sistemaView = new SistemaView();
@@ -65,6 +71,53 @@ var accesosRouter = Backbone.Router.extend({
     this.sistemaRolView.sistemaId = sistema_id;
     this.sistemaRolView.tablaRol.listar(sistema_id);
     this.sistemaRolView.tablaRolPermiso.sistemaId = sistema_id;
+  },
+  //usuario
+  usuarioIndex: function(){
+    if(this.usuarioView == null){
+      this.usuarioView = new UsuarioView();
+    }
+    this.usuarioView.render();
+    this.usuarioView.tablaUsuario.listar();
+  },
+  usuarioLog: function(usuario_id){
+    if(this.usuarioLogView == null){
+      this.usuarioLogView = new UsuarioLogView(dataUsuarioLogView);
+    }
+    this.usuarioLogView.render();
+    //this.sistemaRolView.usuarioId = usuario_id;
+    //this.usuarioLogView.tablaUsuario.listar();
+  },
+  usuarioDetalle: function(usuario_id){
+    if(this.usuarioDetalleView == null){
+      this.usuarioDetalleView = new UsuarioDetalleView(dataUsuarioDetalleView);
+    }
+    this.usuarioDetalleView.set("usuario_id", usuario_id);
+    this.usuarioDetalleView.llenarSelect();
+    this.usuarioDetalleView.obtenerUsuarioCorreo();
+    this.usuarioDetalleView.context.usuario = this.usuarioDetalleView.model;
+    this.usuarioDetalleView.context.estados = this.usuarioDetalleView.estadoUsuariosSelect.toJSON();
+    this.usuarioDetalleView.render();
+  },
+  usuarioSistema: function(usuario_id){
+    if(this.usuarioSistemaView == null){
+      this.usuarioSistemaView = new UsuarioSistemaView(dataUsuarioSistemaView);
+    }
+    this.usuarioSistemaView.render();
+    this.usuarioSistemaView.tablaSistema.urlListar = 
+      limpiarURL(BASE_URL + "usuario/sistema/" , usuario_id);
+    this.usuarioSistemaView.usuarioId = usuario_id;
+    this.usuarioSistemaView.tablaSistema.listar(usuario_id);
+    this.usuarioSistemaView.tablaSistema.usuarioId = usuario_id;
+  },
+  usuarioRolPermiso: function(usuario_id){
+    if(this.usuarioRolPermisoView == null){
+      this.usuarioRolPermisoView = new UsuarioRolPermisoView(dataUsuarioRolPermisoView);
+    }
+    this.usuarioRolPermisoView.set("usuario_id", usuario_id);
+    this.usuarioRolPermisoView.context.sistemas = this.usuarioRolPermisoView.cbmSistemas();
+    this.usuarioRolPermisoView.context.usuario_id = usuario_id;
+    this.usuarioRolPermisoView.render();
   },
 });
 
